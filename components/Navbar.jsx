f64a9f34da6fc63ffab40eabaf154c445b26ca88
dev_ashishpal <ashishpal438@gmail.com>
2025-02-03 01:03:45 +0530

    changed title name

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
               <Image className="h-10 w-auto" src={logo} alt="PropertyPulse" />
 
               <span className="hidden md:block text-white text-2xl font-bold ml-2">
-                PropertyPulse
+                Hoster
               </span>
             </a>
             {/* <!-- Desktop Menu Hidden below md screens --> */}
